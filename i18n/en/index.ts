ac95557cd9ac5f4d0ff3231eed8b083ff2979fd0
Zhou Hua <zhou--hua@163.com>
2024-04-01 16:11:27 +0800

    init

--- i18n/en/index.ts
+++
@@ -1,8 +0,0 @@
-import type { BaseTranslation } from "../i18n-types";
-
-const en: BaseTranslation = {
-	// TODO: your translations go here
-	invalid: "Invalid YAML format.",
-};
-
-export default en;
