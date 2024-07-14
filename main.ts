74972b34d7961a3455f3c4a958904cb3329d3840
fyears <1142836+fyears@users.noreply.github.com>
2024-07-14 12:57:20 +0000

    destory

--- main.ts
+++ main.ts
@@ -2,9 +2,14 @@ import { Plugin } from "obsidian";
 import VConsole from "vconsole";
 
 export default class VConsolePlugin extends Plugin {
+	vConsole?: VConsole;
 	async onload() {
-		new VConsole();
+		this.vConsole = new VConsole();
 	}
 
-	onunload() {}
+	onunload() {
+		if (this.vConsole !== undefined) {
+			this.vConsole.destroy();
+		}
+	}
 }
